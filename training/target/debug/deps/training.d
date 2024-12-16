c94b25706cd9a2acd1bf7c87252502aa89d3878c
unknown <amanuelfekadueba@gmail.com>
2024-12-16 10:57:39 -0900

    dice_roller

---
+++ training/target/debug/deps/training.d
@@ -0,0 +1,5 @@
+C:\Users\Amenzz\Documents\personal Hustle\rust\training\target\debug\deps\training.exe: src/main.rs
+
+C:\Users\Amenzz\Documents\personal Hustle\rust\training\target\debug\deps\training.d: src/main.rs
+
+src/main.rs:
