d76b2e2760cf16f445c32129bddcc6daa1aaefc8
unknown <amanuelfekadueba@gmail.com>
2024-12-16 10:56:58 -0900

    dice rolling

---
+++ dice_roller/target/debug/deps/rand_chacha-7b270920513414a5.d
@@ -0,0 +1,9 @@
+C:\Users\Amenzz\Documents\personal Hustle\rust\dice_roller\target\debug\deps\librand_chacha-7b270920513414a5.rmeta: C:\Users\Amenzz\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\lib.rs C:\Users\Amenzz\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\chacha.rs C:\Users\Amenzz\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\guts.rs
+
+C:\Users\Amenzz\Documents\personal Hustle\rust\dice_roller\target\debug\deps\librand_chacha-7b270920513414a5.rlib: C:\Users\Amenzz\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\lib.rs C:\Users\Amenzz\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\chacha.rs C:\Users\Amenzz\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\guts.rs
+
+C:\Users\Amenzz\Documents\personal Hustle\rust\dice_roller\target\debug\deps\rand_chacha-7b270920513414a5.d: C:\Users\Amenzz\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\lib.rs C:\Users\Amenzz\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\chacha.rs C:\Users\Amenzz\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\guts.rs
+
+C:\Users\Amenzz\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\lib.rs:
+C:\Users\Amenzz\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\chacha.rs:
+C:\Users\Amenzz\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\guts.rs:
---
+++ dice_roller/target/debug/deps/zerocopy_derive-5aacee16d1beb44e.d
@@ -0,0 +1,7 @@
+C:\Users\Amenzz\Documents\personal Hustle\rust\dice_roller\target\debug\deps\zerocopy_derive-5aacee16d1beb44e.dll: C:\Users\Amenzz\.cargo\registry\src\index.crates.io-6f17d22bba15001f\zerocopy-derive-0.7.35\src\lib.rs C:\Users\Amenzz\.cargo\registry\src\index.crates.io-6f17d22bba15001f\zerocopy-derive-0.7.35\src\ext.rs C:\Users\Amenzz\.cargo\registry\src\index.crates.io-6f17d22bba15001f\zerocopy-derive-0.7.35\src\repr.rs
+
+C:\Users\Amenzz\Documents\personal Hustle\rust\dice_roller\target\debug\deps\zerocopy_derive-5aacee16d1beb44e.d: C:\Users\Amenzz\.cargo\registry\src\index.crates.io-6f17d22bba15001f\zerocopy-derive-0.7.35\src\lib.rs C:\Users\Amenzz\.cargo\registry\src\index.crates.io-6f17d22bba15001f\zerocopy-derive-0.7.35\src\ext.rs C:\Users\Amenzz\.cargo\registry\src\index.crates.io-6f17d22bba15001f\zerocopy-derive-0.7.35\src\repr.rs
+
+C:\Users\Amenzz\.cargo\registry\src\index.crates.io-6f17d22bba15001f\zerocopy-derive-0.7.35\src\lib.rs:
+C:\Users\Amenzz\.cargo\registry\src\index.crates.io-6f17d22bba15001f\zerocopy-derive-0.7.35\src\ext.rs:
+C:\Users\Amenzz\.cargo\registry\src\index.crates.io-6f17d22bba15001f\zerocopy-derive-0.7.35\src\repr.rs:
